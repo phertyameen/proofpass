034e9fc3d98a71dd0ae5183bff167cb22b54d70d
phertyameen <aminubabafatima8@gmail.com>
2025-10-13 19:53:50 +0100

    contract deployed to base testnet

---
+++ contract/hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { BASE_SEPOLIA_RPC_URL, BASE_RPC_URL, PRIVATE_KEY, BASESCAN_API_KEY } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    baseSepolia: {
+      url: BASE_SEPOLIA_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 84532,
+    },
+    base: {
+      url: BASE_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 8453,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      baseSepolia: BASESCAN_API_KEY,
+      base: BASESCAN_API_KEY,
+    },
+  },
+};
